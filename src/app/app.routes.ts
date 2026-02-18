10b8745f91389bc0d9049082493a907c1eda3c6e
Rafael V. de Oliveira <elrafael123@gmail.com>
2026-02-18 18:10:31 +0000

    feat: routes and httclient

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Posts } from './pages/posts/posts';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'posts', component: Posts },
+];
