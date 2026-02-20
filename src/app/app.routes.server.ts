e1daa1e15c31499e428f220fd7e44d650961adca
Rafael V. de Oliveira <elrafael123@gmail.com>
2026-02-20 20:09:06 +0000

    fix: add albums route to server routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -14,6 +14,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Client,
   },
   {
+    path: 'albums',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
