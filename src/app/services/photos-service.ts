e5504de743eb37ba48b8de82cc773589affef756
Rafael V. de Oliveira <elrafael123@gmail.com>
2026-04-09 21:27:12 +0100

    feat: add PhotoResponse interfaceto get album title

--- src/app/services/photos-service.ts
+++ src/app/services/photos-service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Photo } from '../shared/interfaces/photo';
 import { Observable } from 'rxjs';
+import { PhotoResponse } from '../shared/interfaces/photo';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +10,7 @@ export class PhotosService {
   private readonly api = '/api/albums';
   private readonly http = inject(HttpClient);
 
-  getPhotos(albumId: string): Observable<Photo[]> {
-    return this.http.get<Photo[]>(`${this.api}/${albumId}/photos`);
+  getPhotos(albumId: string): Observable<PhotoResponse> {
+    return this.http.get<PhotoResponse>(`${this.api}/${albumId}/photos`);
   }
 }
